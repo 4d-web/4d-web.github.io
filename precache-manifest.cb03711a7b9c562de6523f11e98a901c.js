121bab80a49cbbe1085edfa13b223abec23c3414
F0RTI <aybaksua@gmail.com>
2020-09-24 20:41:34 +0300

    new pages

---
+++ precache-manifest.cb03711a7b9c562de6523f11e98a901c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17841b9a732ca43b06a1ba6c0cf01dfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ea5d0c6baa5f3fc541d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1addcbe5d618de7e07a",
+    "url": "/static/js/2.f28b0069.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f28b0069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea5d0c6baa5f3fc541d",
+    "url": "/static/js/main.5f15d312.chunk.js"
+  },
+  {
+    "revision": "2706efc92c6da3fa5049",
+    "url": "/static/js/runtime-main.026315c0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
