9687eda528b586b902c5234ac70ccf06836a0be2
F0RTI <aybaksua@gmail.com>
2020-09-28 00:04:59 +0300

    index page

---
+++ precache-manifest.bbbf6ceac36f2a04a5477eb5a4c291b7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46eed6ac0223bc113c820df4b9ce3bbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e802f857a475eafba1",
+    "url": "/static/css/main.03f67ac7.chunk.css"
+  },
+  {
+    "revision": "fe5732d6f7cc38c29f12",
+    "url": "/static/js/2.3ef02393.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3ef02393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54e802f857a475eafba1",
+    "url": "/static/js/main.6e42b437.chunk.js"
+  },
+  {
+    "revision": "2706efc92c6da3fa5049",
+    "url": "/static/js/runtime-main.026315c0.js"
+  },
+  {
+    "revision": "3309bc970fb9a0c54595de5973a365e9",
+    "url": "/static/media/GothamPro-Black.3309bc97.eot"
+  },
+  {
+    "revision": "d563ad87622a3dfeea9ff9ee22678caa",
+    "url": "/static/media/GothamPro-Black.d563ad87.woff"
+  },
+  {
+    "revision": "efb0c02a5dbe65a7115b477e74c7a661",
+    "url": "/static/media/GothamPro-Black.efb0c02a.ttf"
+  },
+  {
+    "revision": "a3d7d652af07c3610c453b4a51c788c7",
+    "url": "/static/media/GothamPro-Bold.a3d7d652.woff"
+  },
+  {
+    "revision": "c15ee62b232cedc240947b6d814fb750",
+    "url": "/static/media/GothamPro-Bold.c15ee62b.ttf"
+  },
+  {
+    "revision": "ee9b8c537d217fef51d4bca8cf436d7d",
+    "url": "/static/media/GothamPro-Bold.ee9b8c53.eot"
+  },
+  {
+    "revision": "677c3a32938f905b22eb14afd1d5cff4",
+    "url": "/static/media/GothamPro-Light.677c3a32.woff"
+  },
+  {
+    "revision": "7fd3861de823f75a6923000a948c494a",
+    "url": "/static/media/GothamPro-Light.7fd3861d.eot"
+  },
+  {
+    "revision": "abd5115e2ddae490f9cb393fb8cdfc5a",
+    "url": "/static/media/GothamPro-Light.abd5115e.ttf"
+  },
+  {
+    "revision": "6fe466da15644ccc370d3b2ce3a19f6f",
+    "url": "/static/media/GothamPro-Medium.6fe466da.eot"
+  },
+  {
+    "revision": "b86e995a10856a8b8e222fe8ff00a74f",
+    "url": "/static/media/GothamPro-Medium.b86e995a.woff"
+  },
+  {
+    "revision": "c161369173f822acd66d2ff8eea64c52",
+    "url": "/static/media/GothamPro-Medium.c1613691.ttf"
+  },
+  {
+    "revision": "3100f91bbd9e9ca9ecd00255cac7d11c",
+    "url": "/static/media/GothamPro.3100f91b.ttf"
+  },
+  {
+    "revision": "3cb46b372ab77d032576b9f70c83d1ff",
+    "url": "/static/media/GothamPro.3cb46b37.woff"
+  },
+  {
+    "revision": "c31a8e097cb6c2901857d8c686f0063d",
+    "url": "/static/media/GothamPro.c31a8e09.eot"
+  },
+  {
+    "revision": "1d8aadbebdbd53c23037acfe3488debd",
+    "url": "/static/media/index_main_bg.1d8aadbe.jpg"
+  }
+]);
