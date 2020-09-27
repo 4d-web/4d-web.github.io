bc09cfa75746a03f414e8f94cf0b957d1e3815f4
F0RTI <aybaksua@gmail.com>
2020-09-27 18:09:01 +0300

    commit

--- app/src/index.js
+++ app/src/index.js
@@ -14,8 +14,9 @@ import Routes from './data/engine/router'
 //
 // // If you want your app to work offline and load faster, you can change
 // // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
+// // Learn more about service workers: h ttps://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+// 43
 
 import {Router} from 'react-router-dom'
 import {renderRoutes} from 'react-router-config'
