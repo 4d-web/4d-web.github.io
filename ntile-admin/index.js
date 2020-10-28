21803d140b0694b1bf456a6f29cf5804ac3aa122
F0RTI <aybaksua@gmail.com>
2020-10-28 19:05:31 +0200

    ntile

--- ntile-admin/index.js
+++ ntile-admin/index.js
@@ -1,1 +1,34 @@
-console.log("DATA--------------------")
+(function() {
+    'use strict';
+    let kuroir = document.getElementsByClassName("ace-kuroir");
+    console.log("===== THEME ON =====");
+    var style = document.createElement('style');
+    style.innerHTML = `
+.ace-kuroir {
+  background-color: #333 !important;
+  color: white;
+}
+.ace-kuroir .ace_variable {
+  color: #9573A6;
+}
+.ace-kuroir .ace_numeric {
+  color: #6090B4;
+}
+.ace-kuroir .ace_marker-layer .ace_selection {
+  background: rgb(167 129 45 / 57%) !important;
+}
+.ace-kuroir .ace_print-margin {
+  width: 0 !important;
+  background: #2B2B2B;
+}
+#codeBlock {
+  height: 770px !important;
+}
+.ace_cursor {
+  color: white;
+}
+
+`;
+    document.body.appendChild(style);
+    // Your code here...
+})();
