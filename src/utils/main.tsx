13e075daa155e5c6973c6147a96c1b6c24e7c88f
BogdanP <aybaksua@gmail.com>
2022-12-11 20:14:01 +0200

    [fix] cn util

--- src/utils/main.tsx
+++ src/utils/main.tsx
@@ -1,4 +1,3 @@
-// export const cn = (e: (string | ' ' | CSSStyleSheet)[]) => e.join(' ').replace(/\s+/g, ' ').trim();
 export const cn = (e: string | string[] | CSSStyleSheet[]) => {
   if (typeof e !== 'string') return e.join(' ').replace(/\s+/g, ' ').trim();
   if (e.trim() == '') return undefined;
